1d753ac186ab4ac133c4b16c77df9efdd314f9fc
mahendrachilla <154003083+mahendrachilla@users.noreply.github.com>
2024-03-12 17:26:57 +0530

    button

---
+++ javasript1/demo.js
@@ -0,0 +1,12 @@
+Cost count-document.getElementById('count')
+console.log(count.textContent)
+function incr()
+{
+    c++;
+    count.textContent++;
+}
+function decr()
+{
+c--;
+count.textContent--;
+}
